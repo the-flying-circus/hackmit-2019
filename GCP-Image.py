e3cb37a7ff51991fe3d1b8a457d82cae53871f7f
Grey Golla <bobobalink@gmail.com>
2019-09-14 19:49:45 -0400

    finished GCP face emotion tracking and IBM natural language emotion figuring

---
+++ GCP-Image.py
@@ -0,0 +1,35 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+
+def detect_faces_uri(uri):
+    """Detects faces in the file located in Google Cloud Storage or the web."""
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = uri
+
+    response = client.face_detection(image=image)
+    faces = response.face_annotations
+
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+    print('Faces:')
+
+    for face in faces:
+        print(dir(face))
+        print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+        print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+        print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+        print('sorrow: {}'.format(likelihood_name[face.sorrow_likelihood]))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in face.bounding_poly.vertices])
+
+        print('face bounds: {}'.format(','.join(vertices)))
+
+
+if __name__ == '__main__':
+    detect_faces_uri('gs://cloud-samples-data/vision/face/faces.jpeg')
