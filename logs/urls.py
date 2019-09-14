fdc99f4b4b4272c71b007934f06997d64832bb35
Eric Wang <ezwang1@gmail.com>
2019-09-14 16:35:06 -0400

    add weather

--- logs/urls.py
+++ logs/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_nested import routers
 
-from .views import PageViewSet, MetricViewSet
+from .views import PageViewSet, MetricViewSet, weather
 
 
 router = routers.SimpleRouter()
@@ -11,6 +11,8 @@ pages_router = routers.NestedSimpleRouter(router, r'pages', lookup='page')
 pages_router.register(r'metrics', MetricViewSet, base_name='page-metrics')
 
 
-urlpatterns = []
+urlpatterns = [
+    path('weather/', weather)
+]
 urlpatterns += router.urls
 urlpatterns += pages_router.urls
--- logs/views.py
+++ logs/views.py
@@ -1,4 +1,8 @@
+import requests
+
+from django.http import JsonResponse
 from rest_framework import viewsets, permissions
+from django.views.decorators.cache import cache_page
 
 from .models import Page, Metric
 from .serializers import PageSerializer, MetricSerializer
@@ -18,3 +22,11 @@ class MetricViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Metric.objects.filter(page=self.kwargs['page_pk'], page__owner=self.request.user)
+
+
+@cache_page(60)
+def weather(request):
+    resp = requests.get("https://www.metaweather.com/api/location/2367105/")
+    resp.raise_for_status()
+
+    return JsonResponse(resp.json())
