bb375e68efb9ea8511bd5e89a559612b8ef3e9b9
Grey Golla <bobobalink@gmail.com>
2019-09-15 11:11:04 -0400

    fixed log smileys being wrong

--- static/history-carousel.js
+++ static/history-carousel.js
@@ -7,11 +7,13 @@ $(document).ready(function() {
       carouselEl.innerHTML = data.content;
       let emoticon_dict = {};
       for (let i = 0; i < data.metric_set.length; i++) {
-        if (emoticon_dict[data.metric_set[i]["name"] == "anxiety" || emoticon_dict[data.metric_set[i]["name"] == "cynicism"]])
-          emoticon_dict[data.metric_set[i]["name"]] = 5 - data.metric_set[i]["value"];
+        if (data.metric_set[i]["name"] == "anxiety" || data.metric_set[i]["name"] == "cynicism")
+          emoticon_dict[data.metric_set[i]["name"]] = 6 - data.metric_set[i]["value"];
         else
           emoticon_dict[data.metric_set[i]["name"]] = data.metric_set[i]["value"];
       }
+      console.log(emoticon_dict);
+      console.log(data.metric_set);
       document.getElementById("mood-emoticon").innerHTML = `<img src="/static/images/emots/${emoticon_dict['mood']}.png">`;
       document.getElementById("anxiety-emoticon").innerHTML = `<img src="/static/images/emots/${emoticon_dict['anxiety']}.png">`;
       document.getElementById("cynicism-emoticon").innerHTML = `<img src="/static/images/emots/${emoticon_dict['cynicism']}.png">`;
